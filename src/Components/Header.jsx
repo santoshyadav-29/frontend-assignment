5fadccae84d22333cbb3312447b4ddd3bb4aa67a
Santosh Yadav <ymanoutube@gmail.com>
2023-07-20 17:01:32 +0545

    Homepage added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,17 @@
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+   <>
+   <nav className=" flex justify-around items-center p-5">
+    <h1 className="border-2 my-auto border-black rounded-md p-1">Logo</h1>
+    <ul className="flex gap-4 ">
+     
+      <li>Home</li>
+      <li>About us</li>
+      <li>Our Product</li>
+    </ul>
+   </nav>
+   </>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,20 @@
-
+import Header from "../Components/Header"
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="layout min-h-[100vh] bg-[var(--background-color)]">
+      <div className="container mx-auto">
+        <Header />
+        <section className="flex justify-center items-center flex-col">
+          <h1 className="text-4xl font-bold">Welcome to our Homepage</h1>
+          <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+
+          </section>
+          <section>
+            <img src="https://cdn.search.brave.com/serp/v1/static/brand/373c0fe6934b2f59b99a51a3a3c2d5091535c1d19cb361ed6d110ff0c4927b58-brave-logo-light.svg" alt="" />
+          </section>
+        </div>
+
       
     </div>
   )
