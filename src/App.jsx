7dc39fff73a0f700db16e63839d5708df7200caf
Santosh Yadav <ymanoutube@gmail.com>
2023-07-20 20:07:53 +0545

    Project Setup: Configure React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Homepage from "./pages/Homepage"
+import Product from "./pages/Product"
+import Search from "./pages/Search"
+
+
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+      <Search />
+      <Product />
+    </div>
+  )
+}
+
+export default App
